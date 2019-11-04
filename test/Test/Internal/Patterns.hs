d7ecfe42bc9e4bf08653ab5e905a51f3c42e8a43
Sarah Ohlin <sarahohlin1@gmail.com>
2019-11-04 15:55:40 -0500

    added more pattern testing

--- test/Test/Internal/Patterns.hs
+++ test/Test/Internal/Patterns.hs
@@ -29,3 +29,32 @@ test = describe "Patterns" $ do
             it "retrieves a character from a list of possible candidates" $ do
                 runParser (oneOf "abc") "alphabet" `shouldBe` (Right 'a')
                 runParser (oneOf "efg") "dog" `shouldSatisfy` (isLeft)
+        describe "string" $ do
+            it "matches a specific string of text" $ do
+                runParser (string "hello") "hello, world" `shouldBe` (Right "hello")
+                runParser (string "hi, there") "hi there" `shouldSatisfy` (isLeft)
+                runParser (string "hi, there") "hi, there!" `shouldBe` (Right "hi, there")
+        describe "digit" $ do
+            it "matches a single digit" $ do
+                runParser digit "5" `shouldBe` (Right '5')
+                runParser digit "1234" `shouldBe` (Right '1')
+                runParser digit "hello" `shouldSatisfy` (isLeft)
+        describe "natural" $ do
+            it "matches a natural number" $ do
+                runParser natural "123" `shouldBe` (Right "123")
+                runParser natural "hello" `shouldSatisfy` (isLeft)
+                runParser natural "-23" `shouldSatisfy` (isLeft)
+                runParser natural "4" `shouldBe` (Right "4")
+        describe "integer" $ do
+            it "matches a positive / negative integer" $ do
+                runParser integer "-134" `shouldBe` (Right "-134")
+                runParser integer "215.4" `shouldBe` (Right "215")
+                runParser integer "-0" `shouldBe` (Right "-0")
+        describe "number" $ do
+            it "matches any number" $ do
+                runParser number "5" `shouldBe` (Right "5")
+                runParser number "-5" `shouldBe` (Right "-5")
+                runParser number "23.45" `shouldBe` (Right "23.45")
+                runParser number "23." `shouldBe` (Right "23")
+                runParser number "-.23" `shouldBe` (Right "-.23")
+                runParser number ".24" `shouldBe` (Right ".24")
