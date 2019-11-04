95e386df2a4dffda2bfe14f6c81b7b663d1a3e42
Sarah Ohlin <sarahohlin1@gmail.com>
2019-11-04 13:05:15 -0500

    integer parsing

--- src/Internal/Patterns.hs
+++ src/Internal/Patterns.hs
@@ -3,10 +3,18 @@ module Internal.Patterns
 ( item
 , satisfy
 , char
+, oneOf
+, string
+, digit
+, natural
+, integer
+, number
 ) where
     
 import Internal.Parser
 import Internal.Combinators
+import Data.Char
+import Control.Monad (when)
 
 -- | Matches a single char
 item :: Parser Char
@@ -24,3 +32,39 @@ satisfy pred = item >>= \res -> if pred res
 -- | Matches a specific char
 char :: Char -> Parser Char
 char ch = satisfy (==ch)
+
+-- | Matches one of any of the given characters
+oneOf :: [Char] -> Parser Char
+oneOf chars = satisfy (`elem` chars)
+
+-- | Matches a given string
+string :: String -> Parser String
+string "" = pure ""
+string (c:cs) = char c >> string cs >> return (c:cs)
+
+-- | Matches a digit
+digit :: Parser Char
+digit = satisfy isDigit
+
+-- | Matches a whole integer
+natural :: Parser String
+natural = many1 digit
+
+-- | Matches any integer
+integer :: Parser String
+integer = do
+    sign <- optional (char '-')
+    num <- natural
+    return $ sign ++ num
+    
+-- | Matches any number
+number :: Parser String
+number = do
+    sign <- optional (char '-')
+    whole <- natural <|> pure "" 
+    decimal <- ( do
+        pt <- char '.' 
+        fract <- natural
+        return $ pt:fract ) <|> pure "" 
+    when (null whole && null decimal) $ failure "No valid number given."
+    return $ sign ++ whole ++ decimal
