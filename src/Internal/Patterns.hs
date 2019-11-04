562b7b5c8c4861deabaff3322ac4dfada1da8f78
Sarah Ohlin <sarahohlin1@gmail.com>
2019-11-04 11:54:41 -0500

    implemented basic parser patterns

--- src/Internal/Patterns.hs
+++ src/Internal/Patterns.hs
@@ -1,6 +1,25 @@
 -- | Implements basic parser patterns
 module Internal.Patterns
-(   
+( item
+, satisfy
+, char
 ) where
     
 import Internal.Parser
+
+-- | Matches a single char
+item :: Parser Char
+item = Parser $ \input ->
+    if null input
+        then Left "No characters left in stream."
+        else Right (head input, tail input)
+        
+-- | Parses a char matching the given predicate
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy pred = item >>= \res -> if pred res
+                                    then pure res
+                                    else failure $ "Predicate not satisfied on character " ++ [res] ++ "."
+
+-- | Matches a specific char
+char :: Char -> Parser Char
+char ch = satisfy (==ch)
